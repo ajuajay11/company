9fa17f4c61f2800964cae981d63e8fa9c0d3ce3d
ajuajay11 <ajaykrish212121@gmail.com>
2021-11-16 15:24:37 +0530

    .iugugr]

--- js/main.js
+++ js/main.js
@@ -91,3 +91,12 @@ setInterval(() =>{
         fourElement.innerText = count3;
       }
 },100)
+
+function myFunction() {
+  var x= document.getElementById("menu");
+  if(x.style.display == "block"){
+      x.style.display = "none";
+  }else{
+      x.style.display = "block";
+  }
+}
